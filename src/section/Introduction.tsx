ac97fbc642f7de9c17a2e0feca9fb151c29fcdff
DomPatchara <domdoo2015@gmail.com>
2025-04-04 16:23:38 +0700

    update

--- src/section/Introduction.tsx
+++ src/section/Introduction.tsx
@@ -36,12 +36,12 @@ const Introduction = () => {
   return (
     <section className="py-28 lg:py-40">
       <div className="container relative">
-        <div className="sticky top-20">
+        <div className="sticky top-30">
           <div className="flex justify-center">
             <Tag>Introducing Layer</Tag>
           </div>
 
-          <div className="text-4xl md:text-6xl lg:text-7xl text-center font-medium mt-10">
+          <div className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl text-center font-medium mt-10">
             <span>Your creative process deserves better.</span>{" "}
             <span className="text-white/15">
             {words.map((word, index)=>(
